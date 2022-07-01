cde34d6bf05469c811982e6c7fc9dd36a0ee21a9
Andrey <46755642+FortyXD@users.noreply.github.com>
2022-07-01 22:22:10 +0300

    Adding Fullpage.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactFullpage from '@fullpage/react-fullpage';
+import './App.css'
+const anchors = ["About", "secondPage", "Contacts"];
 
-function App() {
-  return (
-    <div>dasdad</div>
-  );
-}
+const App = () => (
+    <ReactFullpage
+        //fullpage options
+        licenseKey={'fsdfldsfldsfl;s'}
+        anchors={anchors}
+        navigation
+        navigationTooltips={anchors}
+
+        scrollingSpeed={1000} /* Options here */
+
+
+        render={({state, fullpageApi}) => {
+            return (
+                <ReactFullpage.Wrapper>
+                    <div className=" section bg-amber-900">
+                        <div className='w-screen h-screen'>
+                            <h1>dadasdasd</h1>
+                        </div>
+                    </div>
+                    <div className="section bg-violet-900">
+                        <p>Section 2</p>
+                    </div>
+                    <div className=" section bg-amber-900">
+                        <div className='w-screen h-screen'>
+                            <h1>dadazasdasdassdasd</h1>
+                        </div>
+                    </div>
+                </ReactFullpage.Wrapper>
+            );
+        }}
+    />
+);
+
 
 export default App;
